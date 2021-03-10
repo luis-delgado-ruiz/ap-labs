9e1363e19cac81b644553652ccb0f61d59947909
Luis Delgado <luisge093@gmail.com>
2021-03-09 20:25:11 -0500

    word reverser lab
    Signed-off-by: Luis Delgado <luisge093@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
+    int i = 0;
+    int len = 0;
+    char ch;
+    char string[100];
+    printf("\nPlease enter the first string\n");
+    while((ch = getchar()) != EOF){
+        if (ch == '\n') {
+            int l = len - 1;
+            for (int j = 0; j < len; j++) { 
+                printf("%c", string[l-j]);
+            }
+            i = 0;
+            len = 0;
+            printf("\n");
+        } 
+        else {
+            string[i] = ch;
+            i++;
+            len++;
+        }
+    }
+    string[i] = '\0';
     return 0;
 }
