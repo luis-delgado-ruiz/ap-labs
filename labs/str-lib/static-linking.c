52d7ccdf626c1230b374a83734bc518c9abb3733
Luis Delgado <luisge093@gmail.com>
2021-03-24 00:25:09 -0500

    str-lib lab
    Signed-off-by: Luis Delgado <luisge093@gmail.com>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    
+    if (strcmp(argv[1], "-add") == 0) {
+        printf("Initial Lenght : %d \n", mystrlen(argv[2]));
+        char *new_string = mystradd(argv[2],argv[3]);
+        printf("New String     : %s \n", new_string);
+        printf("New length     : %d \n", mystrlen(new_string));
+        return 0;
+    }
+
+    if (strcmp(argv[1], "-find") == 0) {
+        int find = mystrfind(argv[2], argv[3]);
+        if (find > -1) {
+            printf("['%s'] string was found at [%d] position \n", argv[3], find);
+        } 
+        else {
+            printf("The strinfg [%s] was not found \n", argv[3]);
+        }
+        return 0;
+    } 
+    else {
+        printf("Action not valid \n");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,51 @@
 
 
 int mystrlen(char *str){
-    return 0;
+
+    int i = 0;
+
+     while (str[i] != '\0'){
+        i++;
+     }
+
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    while(*origin) {
+        origin++;
+    }
+    
+    while(*addition){
+        *origin = *addition;
+        origin++;
+        addition++;
+    }
+    *origin = '\0';
+
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (origin[i] != '\0') {
+        while (origin[i] != substr[0] && origin[i] == substr[j]) {
+            i++;
+            j++;
+        }
+        k = i;
+        if (substr[j] == '\0') {
+            return k;
+        } 
+        else if (origin[i] == '\0') {
+            return 0;
+        }
+        }
+        i = k++;
+        j = 0;
+    return -1;
 }
