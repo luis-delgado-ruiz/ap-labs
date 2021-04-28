dc61f3ff543afaff7d431eb07bb8352871305103
Luis Delgado <luisge093@gmail.com>
2021-04-27 23:57:08 -0500

    File/Directory Monitor
    Signed-off-by: Luis Delgado <luisge093@gmail.com>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,101 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdarg.h>
 #include "logger.h"
+
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		    2
+#define UNDERLINE 	3
+#define BLINK		4
+#define BLACK 		0
+#define RED		    1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+int sysFlag = 0;
+
+void colorText(int attr, int bc) {
+    char command[13];
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr + 30, bc + 40);
+	printf("%s", command);
+}
 
 int initLogger(char *logType) {
     printf("Initializing Logger on: %s\n", logType);
+    if (strcmp(logType, "syslog") == 0){
+        sysFlag = 1;
+    } else{
+        isSyslog = 0;
+        printf("Error [%s] \n", logType);
+    }
     return 0;
 }
 
 int infof(const char *format, ...) {
-    return 0;
+	textcolor(GREEN, BLACK);
+	va_list input;
+	va_start(input, format);
+    if (sysFlag) {
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_INFO, format, input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+	va_end(input);
+    textcolor(WHITE, RESET);
+	return 0;
 }
 
 int warnf(const char *format, ...) {
-    return 0;
+	colorText(YELLOW, BLACK);
+    va_list input;
+    va_start(input, format);
+    if (sysFlag) {
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_WARNING, format, input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+	textcolor(WHITE, RESET);
+	return 0;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
+	colorText(RED, BLACK);
+    va_list input;
+    va_start(input, format);
+    if (sysFlag) {
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_ERR, format, input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+    textcolor(WHITE, RESET);
     return 0;
 }
 
+int panicf(const char *format, ...) {
+    colorText(MAGENTA, BLACK);
+    va_list input;
+    va_start(input, format);
+    if (sysFlag) {
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_CRIT, format, input);
+        closelog();
+    } else {
+        vprintf(format, input);
+    }
+    va_end(input);
+    textcolor(WHITE, RESET);
+    return 0;
+}
